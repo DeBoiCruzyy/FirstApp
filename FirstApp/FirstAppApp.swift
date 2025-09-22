3319db4813e82a02b5d040b954d7f7d0b6aee392
Cruz Arreola <cruzy@Cruzs-Air.lan>
2025-09-22 15:16:37 -0700

    Initial Commit - base project

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Cruz Arreola on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
